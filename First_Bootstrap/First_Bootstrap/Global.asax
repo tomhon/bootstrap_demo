b8a3b111e67fd2278f9b9e4d557c68ea51b7a32d
Tom Honeybone <tomhon@hotmail.com>
2015-07-01 12:13:12 -0700

    First Commit

---
+++ First_Bootstrap/First_Bootstrap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="First_Bootstrap.Global" Language="C#" %>
---
+++ First_Bootstrap/First_Bootstrap/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(First_Bootstrap.Startup))]
+namespace First_Bootstrap
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
